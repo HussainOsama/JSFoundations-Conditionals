d2d9da05a2ed462378f083c090efbde654e3f8c4
HussainOsama <hussain.osama.asd@gmail.com>
2021-02-02 18:14:47 +0300

    Done

--- calculator.js
+++ calculator.js
@@ -1,3 +1,20 @@
 // Define the numbers and the operation here
+let firstNumber = 5
+let secondNumber = 7
+let operation = "*"
+
+if ( operation == "+"){
+    console.log(firstNumber + secondNumber);
+}else if ( operation == "-"){
+    console.log(firstNumber - secondNumber);
+}else if ( operation == "*"){
+    console.log(firstNumber * secondNumber);
+}else if ( operation == "/"){
+    console.log(firstNumber / secondNumber);
+}
+
+
+
 
 // Write if-statements here
+
